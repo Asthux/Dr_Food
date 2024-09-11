e86cb4b503c2049a59296e53f64334871449c345
AsthuX <osm0348@naver.com>
2024-09-11 20:54:19 +0900

    first commit

---
+++ drfood/src/main/java/com/_candoit/drfood/DrfoodApplication.java
@@ -0,0 +1,13 @@
+package com._candoit.drfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrfoodApplication.class, args);
+	}
+
+}
---
+++ drfood/src/test/java/com/_candoit/drfood/DrfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com._candoit.drfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
